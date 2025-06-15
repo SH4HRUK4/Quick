b625f39334bddf5b1e52c2040c4f3e7b5e7d7f28
sharuk <alisharuk649@gmail.com>
2025-06-15 15:32:09 +0300

    updated files

--- app/cart/page.jsx
+++ app/cart/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { assets } from "../../assets";
+import { assets } from "../../assets/assets";
 import OrderSummary from "../../components/OrderSummary";
 import Image from "next/image";
 import Navbar from "../../components/Navbar";
