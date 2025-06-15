e1a606ec05a509ffc726d88a605ec456cef943f3
sharuk <alisharuk649@gmail.com>
2025-06-15 17:53:45 +0300

    updated files

--- config/inngest.js
+++ config/inngest.js
@@ -19,7 +19,7 @@ export const syncedUserCreation = inngest.createFunction(
       event.data;
     const userData = {
       _id: id,
-      email: email_addrresses[0].email_address,
+      email: email_addresses[0].email_address,
       name: first_name + " " + last_name,
       imageUrl: image_url,
     };
