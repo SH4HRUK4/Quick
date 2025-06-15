cb6dfd009c44fe42a7747cd5424306139f6ae633
sharuk <alisharuk649@gmail.com>
2025-06-15 17:57:35 +0300

    updated files

--- config/inngest.js
+++ config/inngest.js
@@ -15,7 +15,7 @@ export const syncedUserCreation = inngest.createFunction(
     event: "clerk/user.created",
   },
   async ({ event }) => {
-    const { id, first_name, last_name, email_addrresses, image_url } =
+    const { id, first_name, last_name, email_addresses, image_url } =
       event.data;
     const userData = {
       _id: id,
